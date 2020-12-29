f60c6db31ae64dab08392bebb9a219e48507f701
LDmeetsGH <72813829+LDmeetsGH@users.noreply.github.com>
2020-12-29 14:12:52 +0100

    Add files via upload

---
+++ ConfirmationPHP.php
@@ -0,0 +1,24 @@
+﻿<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'lars.donart@freenet.de';
+
+    $email_sbuject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "lars.donart@googlemail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email,$email_body,$headers);
+
+    header("Location: Confirmation2.0.html");
+    
+?>
